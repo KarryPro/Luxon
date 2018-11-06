a244f54eda42ea5d5af0d673f8c947a865cc5ef9
Karyna Prodan <32926083+KarryPro@users.noreply.github.com>
2018-11-06 16:29:03 +0200

    Update

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,49 @@
-$("document").ready(function(){$(document).on("click",'a[href^="#"]',function(t){t.preventDefault(),$("html, body").animate({scrollTop:$($.attr(this,"href")).offset().top},500)}),$(function(){$("#typed").typed({strings:["светильник","фонарь","лампа"],typeSpeed:220,attr:"placeholder",bindInputFocusEvents:!0,loop:!0,fadeOut:!0})}),$(".dropdown-menu").on("click",function(t){$(this).hasClass("dropdown-menu-form")&&t.stopPropagation()}),$("#myCarousel").carousel({interval:!1}),$("[id^=carousel-selector-]").click(function(){var t=this.id.substr(this.id.lastIndexOf("-")+1);t=parseInt(t);$("#myCarousel").carousel(t),$(this).addClass("unfade"),$(".thumbnail").not(this).removeClass("unfade")})});
+$("document").ready(function() {
+
+   //Smooth scrolling links
+    $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+	//Making effect of search typing
+	$(function(){
+	   $("#typed").typed({
+	   	strings: ['светильник', 'фонарь', 'лампа'],
+	   	typeSpeed: 220,
+	   	attr: 'placeholder',
+	   	bindInputFocusEvents: true,
+	   	loop: true,
+	   	fadeOut: true
+	   });
+	});
+
+	// Prevent Bootstrap menu from closing
+  	$('.dropdown-menu').on('click', function(e) {
+      if($(this).hasClass('dropdown-menu-form')) {
+          e.stopPropagation();
+      }
+  	});
+
+  	//Bootstrap slider
+    $('#myCarousel').carousel({
+        interval: false
+    });
+ 
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click( function(){
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+        /*$(this).toggleClass('fade').siblings().removeClass('fade');*/
+        $(this).addClass('unfade');
+        $('.thumbnail').not(this).removeClass('unfade');
+    });	
+
+});
+
+
+
